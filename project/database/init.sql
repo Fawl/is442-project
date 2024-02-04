7607e2fa6e8926bab4f50ab5973426537295219c
Jared Song <jskyejet@gmail.com>
2024-02-04 11:22:15 +0800

    fix SQL

--- project/database/init.sql
+++ project/database/init.sql
@@ -1,43 +1,41 @@
-CREATE TYPE "UserType" AS ENUM (
+CREATE TYPE "usertype" AS ENUM (
   'customer',
   'event_manager',
   'ticket_officer'
 );
 
-CREATE TABLE "User" (
+CREATE TABLE "user" (
   "email" varchar(255) PRIMARY KEY NOT NULL,
   "password_hash" char(32) NOT NULL,
-  "user_type" "UserType" NOT NULL,
+  "user_type" usertype NOT NULL,
   "balance" float NOT NULL DEFAULT 1000
 );
 
-CREATE TABLE "Events" (
+CREATE TABLE "ticketedevent" (
   "id" integer PRIMARY KEY,
   "title" varchar(255) NOT NULL,
   "venue" varchar(255) NOT NULL,
   "num_tickets" int NOT NULL,
   "cancelled" bool NOT NULL,
-  "start_time" date NOT NULL,
-  "end_time" date NOT NULL
+  "start_time" timestamp NOT NULL,
+  "end_time" timestamp NOT NULL
 );
 
-CREATE TABLE "Ticket" (
-  "event_id" integer NOT NULL,
+CREATE TABLE "ticket" (
+  "event_id" integer PRIMARY KEY NOT NULL,
   "id" integer NOT NULL,
   "price" float NOT NULL,
-  "redeemed" bool NOT NULL DEFAULT false,
-  PRIMARY KEY ("event_id", "id")
+  "redeemed" bool NOT NULL DEFAULT false
 );
 
-CREATE TABLE "TicketPurchase" (
-  "ticket_id" integer NOT NULL,
+CREATE TABLE "purchase" (
+  "ticket_id" integer PRIMARY KEY NOT NULL,
   "event_id" integer NOT NULL,
-  "user_email" varchar(255) NOT NULL,
-  PRIMARY KEY ("ticket_id", "event_id", "user_email")
+  "user_email" varchar(255) NOT NULL
 );
 
-ALTER TABLE "Ticket" ADD CONSTRAINT "ticket_event_fk" FOREIGN KEY ("event_id") REFERENCES "Events" ("id");
+ALTER TABLE "ticket" ADD CONSTRAINT "ticket_event_fk" FOREIGN KEY ("event_id") REFERENCES "ticketedevent" ("id");
 
-ALTER TABLE "Ticket" ADD CONSTRAINT "ticket_ticketpurchase_fk" FOREIGN KEY ("event_id", "id") REFERENCES "TicketPurchase" ("event_id", "ticket_id");
+ALTER TABLE "ticket" ADD CONSTRAINT "ticket_ticketpurchase_fk" FOREIGN KEY ("id") REFERENCES "purchase" ("ticket_id");
 
-ALTER TABLE "TicketPurchase" ADD CONSTRAINT "user_ticketpurchase_fk" FOREIGN KEY ("user_email") REFERENCES "User" ("email");
+ALTER TABLE "purchase" ADD CONSTRAINT "user_ticketpurchase_fk" FOREIGN KEY ("user_email") REFERENCES "user" ("email");
