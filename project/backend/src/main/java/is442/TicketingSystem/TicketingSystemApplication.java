8430a916ebe6bd9ced109e4ab88076a9b1994bc4
a. darryl <akeeladarryl@gmail.com>
2024-02-21 21:04:01 +0800

    allow all origin

--- project/backend/src/main/java/is442/TicketingSystem/TicketingSystemApplication.java
+++ project/backend/src/main/java/is442/TicketingSystem/TicketingSystemApplication.java
@@ -2,6 +2,9 @@ package is442.TicketingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TicketingSystemApplication {
@@ -9,6 +12,15 @@ public class TicketingSystemApplication {
 	// to make a single jar file
 	public static void main(String[] args) {
 		SpringApplication.run(TicketingSystemApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
