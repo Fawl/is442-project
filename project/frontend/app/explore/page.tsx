39e4c5de1cc8bbcfd37f07b87287a2efa8b45834
a. darryl <akeeladarryl@gmail.com>
2024-02-22 09:34:28 +0800

    added TicketedEvent type, linked up getting TicketedEvents

--- project/frontend/app/explore/page.tsx
+++ project/frontend/app/explore/page.tsx
@@ -1,25 +1,13 @@
-import EventCard from "@/components/cards/event-card";
 import Navbar from "@/components/navigation/navbar";
-import Link from "next/link";
+import EventGallery from "@/components/gallery/event-gallery";
 
-export default function ExplorePage() {
+export default async function ExplorePage() {
   return (
     <>
       <Navbar />
       <main className="min-h-[calc(100dvh-56px)] max-w-[1440px] mx-auto p-4">
         <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6">
-          <Link href="/event/123">
-            <EventCard />
-          </Link>
-          <Link href="/event/123">
-            <EventCard />
-          </Link>
-          <Link href="/event/123">
-            <EventCard />
-          </Link>
-          <Link href="/event/123">
-            <EventCard />
-          </Link>
+          <EventGallery/>
         </div>
       </main>
     </>
--- project/frontend/components/cards/event-card.tsx
+++ project/frontend/components/cards/event-card.tsx
@@ -1,26 +1,39 @@
 import { Badge } from "../ui/badge";
+import { format } from "date-fns";
+import { TicketedEvent } from "@/types";
+interface EventCardProps {
+  event : TicketedEvent
+}
 
-export default function EventCard() {
+const EventCard : React.FC<EventCardProps> = ({event}) => {
+
+  const formattedDate : string = event.start ? format(event.start, "eee, MMMM d, h:mm a") : "TBC";
+  {/* Sat, June 15, 10:00 AM */}
+
   return (
     <div className="hover:cursor-pointer space-y-3">
       <div className="relative">
-        <div className="bg-muted aspect-[16:9] h-[200px] rounded-lg"></div>
+        <div className="bg-muted aspect-[16:9] h-[200px] rounded-lg">
+          <img className="object-cover max-h-full min-w-full" src={event.imageLink ?? "https://images.unsplash.com/photo-1574805950011-8cdf615261b2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NjY5NTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDc4OTIwMTB8&ixlib=rb-4.0.3&q=80&w=1080"} alt="" />
+        </div>
         <Badge className="absolute top-2.5 left-2.5 font-normal bg-indigo-100 text-indigo-800 rounded-md">
           Music
         </Badge>
       </div>
       <div className="space-y-1">
         <div className="text-sm text-[#0977aa] tracking-tight font-medium">
-          Sat, June 15, 10:00 AM
+          {formattedDate}
         </div>
-        <h2 className="font-medium line-clamp-1">Generative AI Hackathon</h2>
+        <h2 className="font-medium line-clamp-1">{event.title ?? "No Event Name"}</h2>
         <p className="text-sm text-muted-foreground line-clamp-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          {event.desc ?? `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          aliquip ex ea commodo consequat.`}
         </p>
       </div>
     </div>
   );
 }
+
+export default EventCard
