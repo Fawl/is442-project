484ca08a5b07aff8c3c1eee5fa11d7d55d3b06af
aaron <aaronlimchengkiat@gmail.com>
2024-02-23 12:56:18 +0800

    FEAT new routes and logic for navbar

--- project/backend/src/main/java/is442/TicketingSystem/controllers/UserController.java
+++ project/backend/src/main/java/is442/TicketingSystem/controllers/UserController.java
@@ -16,47 +16,47 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-        private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-        public UserController(UserRepository userRepository) {
-            this.userRepository = userRepository;
-        }
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        @GetMapping("/all")
-        public List<User> findAll() {
-            return userRepository.findAll();
-        }
+    @GetMapping("/all")
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 
-        @GetMapping("/find")
-        public List<User> findSpecific() {
-            return userRepository.findAll();
-        }
+    @GetMapping("/find")
+    public List<User> findSpecific() {
+        return userRepository.findAll();
+    }
 
-        @PostMapping("/new")
-        public String createUser(@RequestBody User request) {
-            try{
-                userRepository.save(request);
-                return "Success!";
-            }catch( Exception e){
-                return e.toString();
-            }
+    @PostMapping("/new")
+    public String createUser(@RequestBody User request) {
+        try {
+            userRepository.save(request);
+            return "Success!";
+        } catch (Exception e) {
+            return e.toString();
         }
+    }
 
-        @PutMapping("/update")
-        public String updateUser(@RequestBody User request){
-            try{
-                userRepository.save(request);
-                return "Success!";
-            }catch( Exception e){
-                return e.toString();
-            }
+    @PutMapping("/update")
+    public String updateUser(@RequestBody User request) {
+        try {
+            userRepository.save(request);
+            return "Success!";
+        } catch (Exception e) {
+            return e.toString();
         }
+    }
 
-        @DeleteMapping("/delete")
-        public String deleteByEmail(@RequestBody User user) {
-            // TODO: May consider only deleting if user provided is valid
-            userRepository.deleteById(user.getEmail());
-            return "Deleted.";
-        }
-    
+    @DeleteMapping("/delete")
+    public String deleteByEmail(@RequestBody User user) {
+        // TODO: May consider only deleting if user provided is valid
+        userRepository.deleteById(user.getEmail());
+        return "Deleted.";
+    }
+
 }
