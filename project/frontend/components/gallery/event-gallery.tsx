4e2018e28c69ad2e3f6444c25bf42118f23ab396
aaron <aaronlimchengkiat@gmail.com>
2024-02-23 09:45:28 +0800

    refactor

--- project/frontend/components/gallery/event-gallery.tsx
+++ project/frontend/components/gallery/event-gallery.tsx
@@ -1,32 +1,20 @@
 "use client";
 import { TicketedEvent } from "@/types";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import EventCard from "../cards/event-card";
 
-export default function EventGallery() {
-
-  const [events, setEvents] = useState<Array<TicketedEvent>>([])
-  const [loading, setLoading]  = useState(true)
-  const [status, setStatus]  = useState('Loading...')
-
-  useEffect(()=>{
-    fetch(process.env.NEXT_PUBLIC_BACKEND + '/event/all', {method : 'GET'}).then(async res => {
-      setEvents(await res.json() as Array<TicketedEvent>)
-      setLoading(false)
-    }).catch(err => {
-      setStatus('Backend or Database not up')
-    })
-  },[])
-
+export default function EventGallery({ events }: { events: TicketedEvent[] }) {
   return (
-    !loading ?
-    events.map(event => 
-      (<Link href={`/event/${event.id}`} key={event.id}>
-        <EventCard event={event}/>
-      </Link>)
-    )
-    : status
-  )
+    <>
+      {events.length > 0 ? (
+        events.map((event) => (
+          <Link href={`/event/${event.id}`} key={event.id}>
+            <EventCard event={event} />
+          </Link>
+        ))
+      ) : (
+        <>No result</>
+      )}
+    </>
+  );
 }
-
