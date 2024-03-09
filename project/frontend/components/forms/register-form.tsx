51e7b8e551353e3502fe4cd61519c59c37434e5a
aaron <aaronlimchengkiat@gmail.com>
2024-03-10 02:19:38 +0800

    Add new ticket officer modal and update register form

--- project/frontend/components/forms/register-form.tsx
+++ project/frontend/components/forms/register-form.tsx
@@ -19,7 +19,13 @@ import { createUser } from "@/lib/api/user";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
-export default function RegisterForm() {
+export default function RegisterForm({
+  userType = "customer", // DEFAULT VALUE IF NOT PROVIDED
+  isMangerSignUpCallback,
+}: {
+  userType?: "customer" | "ticket_officer" | "event_manager";
+  isMangerSignUpCallback?: () => void;
+}) {
   const router = useRouter();
   const form = useForm<z.infer<typeof RegisterFormSchema>>({
     resolver: zodResolver(RegisterFormSchema),
@@ -30,18 +36,24 @@ export default function RegisterForm() {
   });
 
   const handleOnSubmit = async (data: any) => {
-    // alert(JSON.stringify(data, null, 2));
-
     try {
       const response = await createUser({
         email: data.email,
         password_hash: data.password,
-        user_type: "customer", // REMARKS: CREATE CUSTOMER BY DEFAULT
+        user_type: userType,
       });
       if (response.ok) {
         toast.success("User created successfully");
-        router.push("/login");
-        router.refresh();
+        if (userType === "customer") {
+          router.push("/login");
+          router.refresh();
+        } else {
+          // IF USER TYPE IS TICKET OFFICER
+          if (isMangerSignUpCallback) {
+            isMangerSignUpCallback();
+          }
+          router.refresh();
+        }
       }
     } catch (error: any) {
       toast.error(error.message);
