0f666f604509d9d1f3e081715ebbcbb28fe43c89
aaron <aaronlimchengkiat@gmail.com>
2024-03-10 02:13:30 +0800

    Refactor eventManger root layout

--- project/frontend/app/(event_manager)/layout.tsx
+++ project/frontend/app/(event_manager)/layout.tsx
@@ -1,9 +1,6 @@
 import { authConfig } from "@/auth";
 import Navbar from "@/components/navigation/navbar";
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { getServerSession } from "next-auth";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
@@ -23,17 +20,7 @@ export default async function DashboardLayout({
   return (
     <>
       <Navbar />
-      <div className="sticky top-[56px] border-b flex items-center justify-between h-14 bg-accent px-4 z-50">
-        <Link
-          href="/dashboard"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "text-muted-foreground font-normal"
-          )}
-        >
-          Back to Dashboard
-        </Link>
-      </div>
+
       <div className="min-h-[calc(100dvh-56px)] max-w-[1440px] mx-auto p-4">
         <div className="max-w-7xl mx-auto space-y-6">
           <>{children}</>
