9064e765aaff5eb6d9f711ac875019261f431742
aaron <aaronlimchengkiat@gmail.com>
2024-03-10 02:20:23 +0800

    Update total number of events in DashboardPage

--- project/frontend/app/dashboard/page.tsx
+++ project/frontend/app/dashboard/page.tsx
@@ -9,11 +9,13 @@ export default async function DashboardPage() {
       <div>Something have went wrong!</div>;
     }
 
+    const totalNoOfEvents = response.length;
+
     return (
       <div>
         <div className="space-y-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <InsightSummaryCard title="Total Event" value="1234" />
+            <InsightSummaryCard title="Total Event" value={totalNoOfEvents} />
             <InsightSummaryCard title="Total Revenue" value="$45,231.89" />
             <InsightSummaryCard title="title" value="value" />
           </div>
