c4017f083026ceaaeb9053407b6b120cc139a73d
a. darryl <akeeladarryl@gmail.com>
2024-03-20 14:48:11 +0800

    Cleaning up unused imports

--- project/backend/src/main/java/is442/TicketingSystem/models/EventManager.java
+++ project/backend/src/main/java/is442/TicketingSystem/models/EventManager.java
@@ -3,12 +3,6 @@ package is442.TicketingSystem.models;
 import jakarta.persistence.*;
 
 import java.util.List;
-
-import org.hibernate.annotations.JdbcType;
-import org.hibernate.dialect.PostgreSQLEnumJdbcType;
-
-
-import is442.TicketingSystem.utils.UserType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
